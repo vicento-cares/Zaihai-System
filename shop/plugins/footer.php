7a43f9bc318df475c603e3d32d4bf33bd81a1aab
vicento-cares <143.careappsoftware@gmail.com>
2025-01-06 13:26:54 +0800

    Version Label Update

--- shop/plugins/footer.php
+++ shop/plugins/footer.php
@@ -2,7 +2,7 @@
     <strong>Copyright &copy; 2024. Developed by: Vince Dale Alcantara</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
-      <b>Beta Version</b> 1.0.0
+      <b>Beta Version</b> 1.0.1
     </div>
   </footer>
 <?php
