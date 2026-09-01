2b5a9853b5143ceb834ab2411c68c9e82c761fda
vicento-cares <143.careappsoftware@gmail.com>
2026-09-01 14:28:18 +0800

    Conn Linux Update

--- process/conn_emp_mgt.php
+++ process/conn_emp_mgt.php
@@ -1,10 +1,9 @@
 <?php
 // MS SQL Server Connection
-// $servername_emp_mgt = '172.25.112.131, 1433\SQLEXPRESS'; $username_emp_mgt = 'SA'; $password_emp_mgt = 'SystemGroup2018';
 $servername_emp_mgt = '172.25.116.188'; $username_emp_mgt = 'SA'; $password_emp_mgt = 'SystemGroup@2022';
 
 try {
-    $conn_emp_mgt = new PDO ("sqlsrv:Server=$servername_emp_mgt;Database=emp_mgt_db",$username_emp_mgt,$password_emp_mgt);
+    $conn_emp_mgt = new PDO ("sqlsrv:Server=$servername_emp_mgt;Database=emp_mgt_db;TrustServerCertificate=1",$username_emp_mgt,$password_emp_mgt);
     $conn_emp_mgt->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo 'NO CONNECTION'.$e->getMessage();
